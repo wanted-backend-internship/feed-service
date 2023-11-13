37bdb899a88b56fe3c3082c8fe3b9f8f5291251e
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-14 02:29:41 +0900

    Feat: 예외 처리 추가

---
+++ feed-service/src/main/java/global/exception/ApiException.java
@@ -0,0 +1,14 @@
+package global.exception;
+
+public class ApiException extends RuntimeException {
+    private final ErrorType errorType;
+
+    public ApiException(ErrorType errorType) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
+}
