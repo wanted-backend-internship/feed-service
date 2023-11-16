5e1f089840e8db34617d0f88c394a7a25d10798b
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-16 17:57:12 +0900

    Rename: Bearer 에 띄어쓰기 추가

--- feed-service/src/main/java/com/example/feedservice/domain/auth/LoginRequest.java
+++ feed-service/src/main/java/com/example/feedservice/domain/auth/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.example.feedservice.domain.auth;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 
     @Schema(description = "이메일", example = "test@test.com")
